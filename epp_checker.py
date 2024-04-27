2dc8860539f7dd1bccecdc5b23db666fccc03ee4
KAORUPC\kaoru <ueda.kaoru.a3@s.gifu-u.ac.jp>
2024-04-27 13:15:52 +0900

    first deproy

--- epp_checker.py
+++ epp_checker.py
@@ -17,15 +17,31 @@ st.set_page_config(
 
 showSidebarNavigation = False
 
-st.title('EPP Checker')
-st.write('**サイドバーからモードを選択してください．**')
+# HTMLを使ってタイトルの色を設定
+st.markdown("""
+<style>
+.title {
+    color: white;
+    font-size: 36px;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# st.title('EPP Checker')
+st.markdown('<p class="title">EPP Checker</p>', unsafe_allow_html=True)
+
+# st.write('**サイドバーからモードを選択してください．**')
+st.markdown('<p style="color:white;">サイドバーからモードを選択してください．</p>',
+            unsafe_allow_html=True)
+
 
 st.snow()
 # st.balloons()
 
-# st.image('Images/adeliae_001.jpg')
+# st.image('Images/penguin_001.jpg')
 
-img_path = 'Images/adeliae_001.jpg'
+img_path = 'Images/penguin_001.jpg'
 with open(img_path, "rb") as image_file:
     encoded = base64.b64encode(image_file.read()).decode()
 st.markdown(
